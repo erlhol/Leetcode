0d414f4ddeef7c202bbe6dcbb23102687029fc11
Erling Holte <erling.holte@outlook.com>
2024-01-09 11:05:10 +0100

    Improve runtime

--- arrays_and_hashing/242-valid_anagram/solution.py
+++ arrays_and_hashing/242-valid_anagram/solution.py
@@ -1,6 +1,21 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return self.s_to_dict(s) == self.s_to_dict(t)
+    # Solution approach:
+    # 1. Create a hashmap (dictionary) for each of the words, s and t
+    # 2. Check if the dictionaries are equal.
 
-    def s_to_dict(self, s):
-        return {i: s.count(i) for i in s}
+    # Time complexity: O(n)
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_char_dict = self.create_map(s)
+        t_char_dict = self.create_map(t)
+
+        return s_char_dict == t_char_dict
+
+    def create_map(self, s):
+        char_dict = dict()
+        for char in s:
+            if char not in char_dict:
+                char_dict[char] = 1
+            else:
+                char_dict[char] += 1
+        return char_dict
