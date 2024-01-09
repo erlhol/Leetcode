38a997c422e3cc341c045577b98ff155cb8ae1fc
Erling Holte <erling.holte@outlook.com>
2024-01-09 11:31:28 +0100

    Refactor and add explaination

--- arrays_and_hashing/1-two_sum/solution.py
+++ arrays_and_hashing/1-two_sum/solution.py
@@ -1,14 +1,16 @@
 class Solution:
+    # Solution approach:
+    # Keep track of all the numbers: (I call it values)
+    # Check if there is a value previously found that mathes with the current value (number)
+    # Return the indices immediately if found, else keep going
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numbers = dict()
-        i = 1
-        while i < len(nums):
-            n = nums[i - 1]
-            numbers[n] = i - 1
+        values = dict()
+        for i in range(1, len(nums)):
+            previous_value = nums[i - 1]
+            values[previous_value] = i - 1
 
-            p = nums[i]
-            to_reach = target - p
-            if to_reach in numbers:
-                return [numbers[to_reach], i]
-
-            i += 1
+            current_value = nums[i]
+            to_reach = target - current_value
+            if to_reach in values:
+                return [values[to_reach], i]
