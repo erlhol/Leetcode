2107156b40c093a6620de78968b52462147503e3
Erling Holte <erling.holte@outlook.com>
2024-01-09 17:05:36 +0100

    Add documentation, switch to dict()

--- arrays_and_hashing/217-contains_duplicate/solution.py
+++ arrays_and_hashing/217-contains_duplicate/solution.py
@@ -1,8 +1,15 @@
 class Solution:
+    # Solution approach:
+    # Iterate through the numbers and add them to a map.
+    # If the number is seen, then return True, else, keep iterating
+
+    # Interesting here is that dictionary is actually faster on this particular
+    # input data. Could have used a set here instead, but it is a little bit slower
+
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
+        seen = dict()
         for n in nums:
             if n in seen:
                 return True
-            seen.add(n)
+            seen[n] = 1
         return False
