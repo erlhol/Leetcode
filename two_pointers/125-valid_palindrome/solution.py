6249d2a18f3d78290b6ebeea5dfdd0f5329874b5
Erling Holte <erling.holte@outlook.com>
2024-01-09 11:59:35 +0100

    Optimize runtime of solution

--- two_pointers/125-valid_palindrome/solution.py
+++ two_pointers/125-valid_palindrome/solution.py
@@ -1,21 +1,21 @@
 class Solution:
     # Solution approach:
-    # 1. Iterate over each charachter and create a string of it, only adding alphanumeric or numeric
-    # 2. Then use a two pointer approach to see if each of ends of the strings are equal.
+    # Use a two pointer approach to see if each of ends of the strings are equal.
+    # Increment the pointers while it is not a number or alphanumeric charachter
     # If they are not equal, it is not a palindrome.
 
     # Time complexity: O(n)
 
     def isPalindrome(self, s: str) -> bool:
-        t = s.lower()
-        text = ""
-        for c in t:
-            if c.isalpha() or c.isnumeric():
-                text += c
         low = 0
-        high = len(text) - 1
+        high = len(s) - 1
         while low < high:
-            if text[low] != text[high]:
+            while low < high and not (s[low].isalpha() or s[low].isnumeric()):
+                low += 1
+            while low < high and not (s[high].isalpha() or s[high].isnumeric()):
+                high -= 1
+
+            if s[low].lower() != s[high].lower():
                 return False
             low += 1
             high -= 1
