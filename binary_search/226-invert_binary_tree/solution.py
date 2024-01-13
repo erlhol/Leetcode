e2c204e8c619907d06a142587490917375742d65
Erling Holte <erling.holte@outlook.com>
2024-01-13 13:16:15 +0100

    Add solution

---
+++ binary_search/226-invert_binary_tree/solution.py
@@ -0,0 +1,28 @@
+from collections import deque
+class Solution:
+    # Solution explaination:
+    # Do a BFS search of the tree. But add the right child before the left child (to invert the tree)
+    # Keep track of the previous node, and where it "came from". If it was from a right branch, it should now
+    # be the left branch
+
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        queue = deque([(root,None,"none")]) # The node and the previous
+        while queue:
+            curr, prev, direction = queue.popleft()
+            if direction == "right":
+                prev.left = curr
+            elif direction == "left":
+                prev.right = curr
+                
+            # Add the left and right node to the queue
+            # In this specific order is really important
+            if curr != None:
+                if curr.right != None:
+                    v_right = curr.right
+                    curr.right = None
+                    queue.append((v_right,curr,"right"))
+                if curr.left != None:
+                    v_left = curr.left
+                    curr.left = None
+                    queue.append((v_left,curr,"left"))
+        return root
