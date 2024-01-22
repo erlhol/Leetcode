8787ca90e22e5c7b77a56dabf275dfd13edea28d
Erling Holte <erling.holte@outlook.com>
2024-01-22 21:19:15 +0100

    Remove "add neighbour step"

--- graphs/200-number_of_islands/solution.py
+++ graphs/200-number_of_islands/solution.py
@@ -1,56 +1,27 @@
 class Solution:
     # Solution explaination:
-    # 1. Set the correct neighbours
-    # 2. Traverse each node, and check how many such iterations we have of unvisited nodes
+    # Traverse each node, and check how many such iterations we have of unvisited nodes
+    # Use recursion
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        graph = self.connect_nodes(grid)
-        visited = set()
+        X = len(grid)
+        Y = len(grid[0])
         count = 0
-        for coord in graph:
-            if coord not in visited:
-                visited.update(self.DFS(graph, coord))
-                count += 1
-
+        for x in range(X):
+            for y in range(Y):
+                if grid[x][y] == "1":
+                    self.DFS(grid, x, y, X, Y)
+                    count += 1
         return count
 
-    def DFS(self, G, start):
-        stack = [start]
-        visited = {start}
-        while stack:
-            v = stack.pop()
-            for u in G[v]:
-                if u not in visited:
-                    visited.add(u)
-                    stack.append(u)
-        return visited
-
-    def connect_nodes(self, grid):
-        G = dict()
-        x = 0
-        y = 0
-        for row in grid:
-            for element in row:
-                boundaries = len(grid), len(row)
-                if element == "1":
-                    G[(x, y)] = set()
-                    self.assign_neighbours(grid, x, y, G, boundaries)
-                y += 1
-            x += 1
-            y = 0
-        return G
-
-    def assign_neighbours(self, grid, x, y, graph, boundaries):
+    def DFS(self, grid, x, y, X, Y):
+        if x < 0 or y < 0 or x >= X or y >= Y or grid[x][y] != "1":
+            return
+        grid[x][y] = "x"  # Add as seen
         neighbours = self.find_neighbours(x, y)
         for neighbour in neighbours:
-            x_neigh, y_neigh = neighbour
-            x_boundary, y_boundary = boundaries
-            if x_neigh < 0 or x_neigh >= x_boundary:
-                continue
-            if y_neigh < 0 or y_neigh >= y_boundary:
-                continue
-            if grid[x_neigh][y_neigh] == "1":
-                graph[(x, y)].add((x_neigh, y_neigh))
+            n_x, n_y = neighbour
+            self.DFS(grid, n_x, n_y, X, Y)
 
     def find_neighbours(self, x, y):
         # A node have in total four neighbours
